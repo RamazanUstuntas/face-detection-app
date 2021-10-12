79f3fb380e20af40bbda3c70e1d1d878cd3afbb7
RamazanUstuntas <ramazan-ustuntas@hotmail.com>
2021-10-13 00:14:28 +0300

    Added login, logout and registration forms.

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -6,17 +6,21 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }}>
-        <div className="Tilt-inner pa3">
-         <img style={{paddingTop: '5px'}} alt='logo' src={gandalf}/> 
-        </div> 
-      </Tilt>
-      <Tilt className="Tillt br2 mb3 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 150 }}>
-        <div className='Tilt br2 shadow-2'>
-          <p className='ba2 ma2'>"A WIZARD IS NEVER LATE, NOR IS HE EARLY. HE ARRIVES PRECISELY WHEN HE MEANS TO."</p>
-        </div>
-      </Tilt>
+    <div className='ma2 mt0 flex'>
+      <div className='ma3 mt0'>
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }}>
+          <div className="Tilt-inner pa3">
+           <img style={{paddingTop: '5px'}} alt='logo' src={gandalf}/> 
+          </div> 
+        </Tilt>
+      </div>
+      <div className='mr5 mt0'>
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 'auto', width: 200 }}>
+          <div className='Tilt br2 shadow-2'>
+            <p className='ma2'>"A WIZARD IS NEVER LATE, NOR IS HE EARLY. HE ARRIVES PRECISELY WHEN HE MEANS TO."</p>
+          </div>
+        </Tilt>
+      </div>
     </div>
   )
 }
